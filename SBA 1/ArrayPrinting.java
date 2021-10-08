6975ac037fc383107b14f6404ea15e70c83226a1
fahiz-github <65487419+fahiz-github@users.noreply.github.com>
2021-10-08 12:04:08 +0530

    Update ArrayPrinting.java

--- SBA 1/ArrayPrinting.java
+++ SBA 1/ArrayPrinting.java
@@ -1,1 +1,31 @@
+package com.ust.test;
+
+public class ArrayPrinting {
+
+	public static void main(String[] args) {
+		int array[]= {10,20,12,40,74,53,84,47,520,98};
+		for(int i:array) {
+			System.out.println(i);
+		}
+		
+
+	
+
+}
+}
+/* 
+ * output
+10
+20
+12
+40
+74
+53
+84
+47
+520
+98
+*/
+
+
 
