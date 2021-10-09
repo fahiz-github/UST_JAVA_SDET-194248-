d82d6199530a1e981c50cf88020c223fb9159eaa
fahiz-github <65487419+fahiz-github@users.noreply.github.com>
2021-10-09 20:06:39 +0530

    Add files via upload

--- Java Programs/AbstractExample1.java
+++ Java Programs/AbstractExample1.java
@@ -1,1 +1,18 @@
+package com.ust.test;
+abstract class Base {
+ abstract void fun();
+}
+class Derived extends Base {
+void fun()
+  {
+	System.out.println("Derived fun() called");
+  }
+}
 
+public class AbstractExample1 {
+	public static void main(String args[])
+	{
+	Base b = new Derived();
+	b.fun();
+
+}}
