20b25b207ee6cd6db9da7539fc78115766737b28
fahiz-github <65487419+fahiz-github@users.noreply.github.com>
2021-10-28 10:06:40 +0530

    Add files via upload

---
+++ Cypress/ex1.spec.js
@@ -0,0 +1,10 @@
+// test suite name
+
+describe('simple test', function () {
+    // Test case
+       it('Scenario 1', function (){
+          // test step for URL launching
+          cy.visit("https://www.instagram.com/accounts/login/");
+       });
+    });
+    
